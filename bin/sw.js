82e4d8170db718cf9b0815a55c33f1cb86c0fd2a
wan9shu <203435392@qq.com>
2021-05-20 11:18:17 +0800

    add log

--- bin/sw.js
+++ bin/sw.js
@@ -7,14 +7,30 @@ const [, , ...argvs] = process.argv;
 (async function () {
   if (argvs[0] == 'git') {
     if (argvs[1] == 'config') {
-      if (argvs[2]) {
-        await runGitConfig (argvs[2]);
+      if (argvs[2] == 'log') {
+        await log ();
+      } else if (argvs[2]) {
+        await config (argvs[2]);
       }
     }
   }
 }) ();
-
-async function runGitConfig (name) {
+async function log () {
+  try {
+    const {stdout} = await exec (
+      [
+        `git config user.name`,
+        `git config user.email`,
+        `git config --global user.name`,
+        `git config --global user.email`,
+      ].join ('&')
+    );
+    console.log (stdout);
+  } catch (e) {
+    console.log (e);
+  }
+}
+async function config (name) {
   const email = gitConfig[name];
   try {
     await exec (
